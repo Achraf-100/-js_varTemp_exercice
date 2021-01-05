a16fbf35807a7a169260d6431ba684900196fb94
Achraf <hardy1348@live.fr>
2021-01-05 10:09:26 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,61 @@
+
+// Exo 1
+// let a = 1; 
+// let b = 2; 
+// let c = 3; 
+// let d = 4; 
+
+// let tempo = a
+// let tempoo = b
+// a = d;
+// b = c;
+// c = tempoo;
+// d = tempo;
+// console.log(a, b, c, d);
+
+// Exo 2
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let d = 4;
+
+// let temp = a;
+// a = b;
+// b = c;
+// c = d;
+// d = temp;
+// console.log(a, b, c, d);
+
+// Exo 3
+
+// let tableau = [3, 2, 1];
+// console.log(tableau);
+
+// tableau.unshift(4);
+// console.log(tableau);
+
+// Exo 4
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let e = 5;
+let f = 6;
+
+let temp = a;
+let tempp = b;
+let tempo = c;
+a = d;
+b = e;
+c = temp;
+d = tempp;
+e = f;
+f = tempo;
+
+console.log(a, b, c, d, e, f);
+
+
+
+
+
